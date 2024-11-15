ac2847a7d66ef0d5bdc684f0f1fa1cbcd1db3f21
sebastian <sebastian@sommerfeld.io>
2024-11-15 14:23:28 +0000

    initial vagrantbox setup

---
+++ Vagrantfile
@@ -0,0 +1,23 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+BOX_NAME = "bento/ubuntu-24.04" # https://portal.cloud.hashicorp.com/vagrant/discover/bento/ubuntu-24.04
+VM_HOSTNAME = "talos"
+
+Vagrant.configure("2") do |config|
+  config.vm.box = BOX_NAME
+  config.vm.hostname = VM_HOSTNAME
+
+  config.vm.provider "virtualbox" do |v|
+    v.memory = "4096"
+    v.cpus = 2
+    v.name = VM_HOSTNAME
+  end
+
+  config.vm.provision "shell", path: "provision/bootstrap.sh"
+  config.vm.provision "shell", path: "provision/bootstrap-vagrant.sh", privileged: false
+
+  (7000..7080).each do |port|
+    config.vm.network "forwarded_port", guest: port, host: port
+  end
+end
---
+++ provision/bootstrap.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+# @file bootstrap.sh
+# @brief Bootstrap script to provision the Vagrantbox and to install into non-virtual Ubuntu systems.
+# @description
+#   This script is used to provision the Vagrantbox and to install into non-virtual Ubuntu systems.
+
+# shellcheck disable=SC1091
+
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
+
+echo "[INFO] === Environment ========================================"
+echo "User     = $USER"
+echo "Hostname = $HOSTNAME"
+echo "Home dir = $HOME"
+hostnamectl
+echo "[INFO] ========================================================"
+
+
+echo "[INFO] Update apt cache"
+sudo apt-get update
+
+
+echo "[INFO] Install packages and tools"
+sudo apt-get install -y apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release \
+  auditd \
+  git \
+  tilix \
+  tmux \
+  vim \
+  ncdu \
+  neofetch \
+  htop \
+  jq
+sudo snap install --classic code
+
+
+echo "[INFO] Install Docker"
+
+sudo apt-get update
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install -y docker-ce \
+  docker-ce-cli \
+  containerd.io \
+  docker-buildx-plugin \
+  docker-compose-plugin
+
+if ! getent group docker > /dev/null; then
+    sudo groupadd docker
+fi
+
+if id "vagrant" &>/dev/null; then
+  sudo usermod -aG docker vagrant
+fi
+sudo usermod -aG docker "$USER"
+newgrp docker
+
+echo "[INFO] Test docker"
+docker run --rm hello-world:latest
+
+
+echo "[INFO] Install k9s"
+readonly K9S_VERSION="0.32.5"
+curl -fsSL -o /tmp/k9s.deb https://github.com/derailed/k9s/releases/download/v$K9S_VERSION/k9s_linux_amd64.deb
+sudo apt-get install -y /tmp/k9s.deb
+rm /tmp/k9s.deb
+
+
+echo "[INFO] Install ArgoCD Autopilot"
+VERSION=$(curl --silent "https://api.github.com/repos/argoproj-labs/argocd-autopilot/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
+curl -fsSL --output - "https://github.com/argoproj-labs/argocd-autopilot/releases/download/$VERSION/argocd-autopilot-linux-amd64.tar.gz" | tar zx
+mv ./argocd-autopilot-* /usr/local/bin/argocd-autopilot
+
+
+echo "[INFO] Cleanup"
+sudo apt-get clean
+sudo rm -rf /var/lib/apt/lists/*
+
+
+echo "[INFO] ========================================================"
+echo "[INFO] Maybe you still need to add the user to the docker group"
+echo "[INFO]   sudo usermod -aG docker \$USER"
+echo "[INFO]"
+echo "[INFO] Documentation"
+echo "[INFO]   https://sommerfeld-io.github.io/vm-ubuntu"
+echo "[INFO] ========================================================"
